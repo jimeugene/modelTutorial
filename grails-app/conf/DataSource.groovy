ef951bd0b7bdc20ce85cac5fc4f0705b4e2b75a7
student <student@debian>
2013-12-09 17:13:42 +0000

    Updated database config

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -15,8 +15,13 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+        dbCreate = "update"
+	driverClassName = "com.mysql.jdbc.Driver"
+	dialect=org.hibernate.dialect.MySQL5Dialect
+	username = "webarch"
+	password = "webarch"
+	url = "jdbc:mysql://localhost/eregister?autoReconnect=true&useUniCode=true&characterEncoding=UTF-8"
+	pooled = true
         }
     }
     test {
